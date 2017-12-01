ff29d6df406dc9148e7d5c0e09adeeb425518762
dmfriendjr <davidmfriendjr@gmail.com>
2017-11-30 21:28:19 -0500

    Made query string be constructed with each query using given search term

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
 
-queryUrl += '?' + $.param({
-  'api-key': "2c0f5d2783db4199ba4947782365ea67",
-  'q': "trump"
-});
-
 function getArticles(searchTerm) {
+	queryUrl += '?' + $.param({
+	  'api-key': "2c0f5d2783db4199ba4947782365ea67",
+	  'q': searchTerm;
+	});
+
+
 	$.ajax({
 		url: queryUrl, 
 		method: 'GET'
