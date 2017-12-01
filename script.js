595e761ad93fb4a4de73a2d3df63a1ff59f737da
dmfriendjr <davidmfriendjr@gmail.com>
2017-11-30 21:29:20 -0500

    Removed wrongly placed semicolon

--- script.js
+++ script.js
@@ -3,9 +3,8 @@ var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
 function getArticles(searchTerm) {
 	queryUrl += '?' + $.param({
 	  'api-key': "2c0f5d2783db4199ba4947782365ea67",
-	  'q': searchTerm;
+	  'q': searchTerm
 	});
-
 
 	$.ajax({
 		url: queryUrl, 
