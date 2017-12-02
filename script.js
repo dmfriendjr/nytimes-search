7ffbf35031309ab8614d7cc4d864e77e6a6cb1ee
dmfriendjr <davidmfriendjr@gmail.com>
2017-12-02 10:16:26 -0500

    Moved query string into get articles function and added on click listener for submit button to get input field

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
 
-queryUrl += '?' + $.param({
-  'api-key': "2c0f5d2783db4199ba4947782365ea67",
-  'q': "trump"
-});
 
 function getArticles(searchTerm) {
+	var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+
+	queryUrl += '?' + $.param({
+  		'api-key': "2c0f5d2783db4199ba4947782365ea67",
+  		'q': searchTerm,
+		'begin_date': '20171010'
+	});
+
 	$.ajax({
 		url: queryUrl, 
 		method: 'GET'
@@ -14,5 +17,7 @@ function getArticles(searchTerm) {
 	});
 }
 
+$('#submit-button').on('click', (event) => {
+	getArticles($('#search-box').val());
+});
 
-getArticles('trump');
